dbb2e3de048623d016786579142f0bfe3dd5c141
Chris Henry <cdhenry@gmail.com>
2018-04-26 15:31:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ $(document).ready(function (){
 function searchRepositories() {
   let term = document.getElementById("searchTerms").value
   const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
+
+function showRepositories() {
+  
 }
 
 function displayError() {
