644ccac2eff63f35ba48cac7509fa9b177780f22
Chris Henry <cdhenry@gmail.com>
2018-04-26 15:35:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ function searchRepositories() {
   req.send()
 }
 
-function showRepositories() {
-
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText)
+  const src = document.getElementById("repository-template").innerHTML
+  const template = Handlebars.compile(src)
+  const repoList = template(repos)
+  document.getElementById("repositories").innerHTML = repoList
 }
 
 function displayError() {
