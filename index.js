6d2e5d0c404d36ab5c1e583c994b0a8fc51b4036
Chris Henry <cdhenry@gmail.com>
2018-04-26 15:39:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ function searchRepositories() {
   }
 }
 
-function showRepositories(event, data) {
-  const repos = JSON.parse(this.responseText)
-  const src = document.getElementById("repository-template").innerHTML
-  const template = Handlebars.compile(src)
-  const repoList = template(repos)
-  document.getElementById("repositories").innerHTML = repoList
-}
+// function showRepositories(event, data) {
+//   const repos = JSON.parse(this.responseText)
+//   const src = document.getElementById("repository-template").innerHTML
+//   const template = Handlebars.compile(src)
+//   const repoList = template(repos)
+//   document.getElementById("repositories").innerHTML = repoList
+// }
 
 function displayError() {
 
@@ -23,7 +23,3 @@ function displayError() {
 function showCommits() {
 
 }
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  Handlebars.registerPartial("authorPartial", document.getElementById("author-partial-template").innerHTML)
-});
