fc711b5e6edb86e6416e5530062d97e1e63b6493
Chris Henry <cdhenry@gmail.com>
2018-04-26 15:20:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ $(document).ready(function (){
 });
 
 function searchRepositories() {
-
+  let term = document.getElementById("term").value
+  debugger;
 }
 
 function displayError() {
-  
+
 }
 
 function showCommits() {
