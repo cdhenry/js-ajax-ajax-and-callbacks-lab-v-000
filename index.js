4d3b0f8698b7b36053bee08275cc8ec89a3983a8
Chris Henry <cdhenry@gmail.com>
2018-04-26 16:09:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(document).ready(function (){
 function searchRepositories() {
   const searchTerms = document.getElementById("searchTerms").value
   $.get(`https://api.github.com/users/${searchTerms}/repos`, function(response) {
-    console.log(response[0].name);
+    console.log(response.map( repository => repository.name );
     $("#results").html();
   }).fail( function(error) {
     $("#errors").html("I'm sorry, there's been an error. Please try again.");
